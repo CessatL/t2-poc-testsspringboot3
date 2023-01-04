f617909421da44f4cb9acccd2ba380a7cecce253
CESSAT Laurent <laurent.cessat_2@allianz.fr>
2023-01-04 15:00:24 +0100

    [CESSATL] - Init Commit

---
+++ src/main/java/fr/allianz/t2/testsspringboot/TestsspringbootApplication.java
@@ -0,0 +1,13 @@
+package fr.allianz.t2.testsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/allianz/t2/testsspringboot/TestsspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.allianz.t2.testsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
